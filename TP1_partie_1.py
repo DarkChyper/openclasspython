2b5c8c958472a367c89f86fa207a881338392e38
sylvainnizac <sylvain.nizac@laposte.net>
2015-04-21 21:58:53 +0200

    TP1 prettier version

--- TP1_partie_1.py
+++ TP1_partie_1.py
@@ -1,28 +1,18 @@
 #!/usr/bin/python3.4
 # -*- coding: utf8 -*-
+
 temp = input('entrez une année : ')
 
 while temp != 'quit':
     try:
         ann = int(temp)
-        if ann%4 == 0:
-            if ann%100 == 0:
-                if ann%400 == 0:
-                    bisextile = True
-                else:
-                    bisextile = False
-            else:
-                bisextile = True
-        else:
-            bisextile = False
-
-        if bisextile:
+    except:
+        print('ce n\'est pas un chiffre mais un(e)', type(temp))
+    else:
+        if (ann%400 == 0) or (ann%4 == 0 and ann%100 != 0):
             print('c\'est une année bisextile!')
         else:
             print('ce n\'est pas une année bisextile!')
-            
-    except:
-        print('ce n\'est pas un chiffre mais un(e)', type(temp))
-    
-    temp = input('entrez une année : ')
+    finally:
+        temp = input('entrez une année : ')
 
