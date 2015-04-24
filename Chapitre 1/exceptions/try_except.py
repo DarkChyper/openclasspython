2397d97ff52a1f369b60199e5df74fd13a01a14c
DarkChyper <contact@simon-lhoir.fr>
2015-04-24 22:48:56 +0200

    Mineure: Correction de try andd except

--- Chapitre 1/exceptions/try_except.py
+++ Chapitre 1/exceptions/try_except.py
@@ -3,16 +3,18 @@
 
 """ Test des try et except complets """
 print "Veuillez entrer un numérateur puis un denompinateur"
+resultat = None
 numerateur = raw_input("Numérateur ? ")
 denominateur = raw_input("Dénominateur ? ")
 try:
-	resultat = numerateur / denominateur
+	resultat = float(numerateur) / float(denominateur)
+	print "Résultat :",resultat
 except ZeroDivisionError:
 	print "Division par zéro impossible"
 except NameError:
 	print "vous n'avez pas définit l'un des opérandes"
 except TypeError:
-	print "Une division ne se fait qu'avec des nombre"
+	print "Une division ne se fait qu'avec des nombres"
 	print "numérateur : ",numerateur
 	print "dénominateur : ",denominateur 
-print numerateur,"/",denominateur,"=",resultat
+
