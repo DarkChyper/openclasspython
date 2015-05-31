b01c43e30f24528ad68490738b64dc1576a94c35
Sylvain NIZAC <sylvain.nizac@laposte.net>
2015-05-31 15:42:23 +0200

    création classes Data et Network

---
+++ roboc/roboc_client_class.py
@@ -0,0 +1,29 @@
+# -*- coding: utf8 -*-
+
+class Data():
+    """
+    Classe contenant les données utiles à tous les Threads
+    """
+    serveur = True
+    hote = "localhost"
+    port = 12800
+    connexion = None
+    
+class Network(Data):
+    """
+    ensemble des méthodes régissant la connection TCP
+    """
+    def __init__(self):
+        """
+        Ouverture de la connexion
+        """
+        connexion = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        connexion_avec_serveur.connect((hote, port))
+        print("Le client est maintenant connecté")
+        
+    def deco(self):
+        """
+        Fermeture de  la connexion principale
+        """
+        print("Fermeture de la connexion")
+        connexion.close()
