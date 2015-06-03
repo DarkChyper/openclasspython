3a6059142a1b9105aed6b33e5e47dfa634521ffe
Sylvain NIZAC <sylvain.nizac@laposte.net>
2015-06-03 13:56:10 +0200

    test d'interface et prefixage message

--- roboc/roboc_client_interf.py
+++ roboc/roboc_client_interf.py
@@ -9,7 +9,9 @@ class Envoi():
         pass
     
     def envoi(message):
-        message = message.encore()
+        if Data.mode == "choix":
+            message = "chx" + message
+        message = message.encode()
         Data.connexion.send(message) 
 
 class Interface(Frame, Envoi):
@@ -20,14 +22,18 @@ class Interface(Frame, Envoi):
     def __init__(self, fenetre, **kwargs):
         Frame.__init__(self, fenetre, width=768, height=576, **kwargs)
         self.pack(fill=BOTH)
-        self.nb_clic = 0
         
         # Création de nos widgets
-        self.message = Label(self, text="Entrez un message pour le serveur :")
-        self.message.pack()
+        #self.message = Label(self, text="Entrez un message pour le serveur :")
+        #self.message.pack()
+        
+        comm = LabelFrame(fenetre, text="Entrez un message pour le serveur :", padx=40, pady=40)
+        comm.pack(fill="both", expand="yes")
+ 
+        #Label(l, text="A l'intérieure de la frame").pack()
         
         self.var_texte = StringVar()
-        ligne_texte = Entry(fenetre, textvariable=self.var_texte, width=5)
+        ligne_texte = Entry(comm, textvariable=self.var_texte, width=5)
         ligne_texte.pack()
         ligne_texte.focus_set()
         
@@ -45,12 +51,8 @@ class Interface(Frame, Envoi):
     
     def cliquer(self):
         """Il y a eu un clic sur le bouton.
-        
         On change la valeur du label message."""
         
-        self.nb_clic += 1
-        #self.message["text"] = "Vous avez cliqué {} fois.".format(self.nb_clic)
-        #Envoi.envoi("envoi reussi {} fois".format(self.nb_clic))
         Envoi.envoi(self.var_texte.get())
         self.var_texte.set("")
 
